5add3194c25508a227c8bf2b01e2d7ae9f1c4976
ASUS <Yehor Kholodiuk>
2022-02-10 14:17:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import PlusButton from "./PlusButton";
 
 function App() {
+  const appClick = () => {
+    console.log('APP CLICK')
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+  <PlusButton
+  buttonText = 'Plus One'
+  buttonOnClick = {appClick}
+  />
+</div>
   );
 }
 
